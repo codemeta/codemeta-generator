8538efd4637baf4f6d8f17e4287b389593b93021
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-10-02 18:10:44 +0200

    Add more fields (datePublished, codeRepository, contIntegration, issueTracker, contributors)

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
  * See top-level LICENSE file for more information
  */
 
+"use strict";
+
 function emptyToUndefined(v) {
     if (v == null || v == "")
         return undefined;
@@ -16,85 +18,120 @@ function getIfSet(query) {
     return emptyToUndefined(document.querySelector(query).value);
 }
 
-function getNbAuthors() {
-    var nbAuthorsField = document.querySelector('#nbAuthors');
-    return parseInt(nbAuthorsField.value, 10);
+function getNbPersons(prefix) {
+    var nbField = document.querySelector(`#${prefix}_nb`);
+    return parseInt(nbField.value, 10);
 }
 
-function addAuthorWithId(authorId) {
+function createPersonFieldset(personPrefix, legend) {
+    // Creates a fieldset containing inputs for informations about a person
     var fieldset = document.createElement("fieldset")
-    fieldset.classList.add("author");
-    fieldset.id = `author_${authorId}`
+    fieldset.classList.add("person");
+    fieldset.id = personPrefix;
     
     fieldset.innerHTML = `
-        <legend>Author #${authorId}</legend>
+        <legend>${legend}</legend>
         <p>
-            <label for="author_givenName_${authorId}">Given name</label>
-            <input type="text" id="author_givenName_${authorId}" name="author_givenName_${authorId}" required="true" />
+            <label for="${personPrefix}_givenName">Given name</label>
+            <input type="text" id="${personPrefix}_givenName" name="${personPrefix}_givenName"
+                placeholder="Jane" required="true" />
         </p>
         <p>
-            <label for="author_familyName_${authorId}">Family name</label>
-            <input type="text" id="author_familyName_${authorId}" name="author_familyName_${authorId}" />
+            <label for="${personPrefix}_familyName">Family name</label>
+            <input type="text" id="${personPrefix}_familyName" name="${personPrefix}_familyName"
+                placeholder="Doe" />
         </p>
         <p>
-            <label for="author_email_${authorId}">E-mail address</label>
-            <input type="email" id="author_email_${authorId}" name="author_email_${authorId}" />
+            <label for="${personPrefix}_email">E-mail address</label>
+            <input type="email" id="${personPrefix}_email" name="${personPrefix}_email"
+                placeholder="jane.doe@example.org" />
         </p>
         <p>
-            <label for="author_id_${authorId}">URI</label>
-            <input type="url" id="author_id_${authorId}" name="author_id_${authorId}" />
+            <label for="${personPrefix}_id">URI</label>
+            <input type="url" id="${personPrefix}_id" name="${personPrefix}_id"
+                placeholder="http://orcid.org/0000-0002-1825-0097" />
         </p>
     `;
 
-    var authorsFieldset = document.querySelector('#authors');
-    authorsFieldset.appendChild(fieldset);
+    return fieldset;
 }
 
-function addAuthor() {
-    var authorId = getNbAuthors() + 1;
+function addPersonWithId(container, legend, prefix, id) {
+    var fieldset = createPersonFieldset(`${prefix}_${id}`, `${legend} #${id}`);
 
-    addAuthorWithId(authorId);
-
-    var nbAuthorsField = document.querySelector('#nbAuthors');
-    nbAuthorsField.value = authorId;
+    container.appendChild(fieldset);
 }
 
-function removeAuthor() {
-    var authorId = getNbAuthors();
+function addPerson(legend, prefix) {
+    var container = document.querySelector(`#${prefix}_container`);
+    var personId = getNbPersons(prefix) + 1;
 
-    document.querySelector(`#author_${authorId}`).remove();
+    addPersonWithId(container, legend, prefix, personId);
 
-    var nbAuthorsField = document.querySelector('#nbAuthors');
-    nbAuthorsField.value = authorId-1;
+    var nbPersonsField = container.querySelector(`#${prefix}_nb`);
+    nbPersonsField.value = personId;
+}
+
+function removePerson(prefix) {
+    var personId = getNbPersons(prefix);
+
+    document.querySelector(`#${prefix}_${personId}`).remove();
+
+    var nbPersonsField = document.querySelector(`#${prefix}_nb`);
+    nbPersonsField.value = personId-1;
+}
+
+// Names of codemeta properties with a matching HTML field name
+const directCodemetaFields = [
+    'codeRepository',
+    'contIntegration',
+    'dateCreated',
+    'datePublished',
+    'dateModified',
+    'issueTracker',
+    'name',
+    'version',
+];
+
+function generatePerson(idPrefix) {
+    return {
+        "@type": "Person",
+        "givenName": getIfSet(`#${idPrefix}_givenName`),
+        "familyName": getIfSet(`#${idPrefix}_familyName`),
+        "email": getIfSet(`#${idPrefix}_email`),
+        "@id": getIfSet(`#${idPrefix}_id`),
+    }
+}
+
+function generatePersons(prefix) {
+    var persons = [];
+    var nbPersons = getNbPersons(prefix);
+
+    for (let personId = 1; personId <= nbPersons; personId++) {
+        persons.push(generatePerson(`${prefix}_${personId}`));
+    }
+
+    return persons;
 }
 
 function generateCodemeta() {
     var inputForm = document.querySelector('#inputForm');
     var codemetaText;
     if (inputForm.checkValidity()) {
-        authors = [];
-
-        var nbAuthors = getNbAuthors();
-
-        for (let authorId = 1; authorId <= nbAuthors; authorId++) {
-            authors.push({
-                "@type": "Person",
-                "givenName": getIfSet(`#author_givenName_${authorId}`),
-                "familyName": getIfSet(`#author_familyName_${authorId}`),
-                "email": getIfSet(`#author_email_${authorId}`),
-                "@id": getIfSet(`#author_id_${authorId}`),
-            })
-        }
-
         var doc = {
             "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
             "@type": "SoftwareSourceCode",
-            "name": getIfSet('#name'),
-            "codeRepository": getIfSet('#codeRepository'),
-            "dateCreated": getIfSet('#dateCreated'),
             "license": "https://spdx.org/licenses/" + getIfSet('#license'),
-            "author": authors,
         };
+
+        directCodemetaFields.forEach(function (item, index) {
+            doc[item] = getIfSet('#' + item)
+        });
+
+        doc = Object.assign(doc, {
+            "author": generatePersons('author'),
+            "contributor": generatePersons('contributor'),
+        });
 
         codemetaText = JSON.stringify(doc, null, 4);
     }
@@ -106,24 +143,22 @@ function generateCodemeta() {
     document.querySelector('#codemetaText').textContent = codemetaText;
 }
 
-function initAuthors() {
-    var nbAuthors = getNbAuthors();
+function initPersons(prefix, legend) {
+    var nbAuthors = getNbPersons(prefix);
+    var personContainer = document.querySelector(`#${prefix}_container`)
 
-    for (let authorId = 1; authorId <= nbAuthors; authorId++) {
-        addAuthorWithId(authorId);
+    for (let personId = 1; personId <= nbAuthors; personId++) {
+        addPersonWithId(personContainer, legend, prefix, personId);
     }
 }
 
 function initCallbacks() {
-    document.querySelector('#addAuthor')
-        .addEventListener('click', addAuthor);
-    document.querySelector('#removeAuthor')
-        .addEventListener('click', removeAuthor);
     document.querySelector('#generateCodemeta')
         .addEventListener('click', generateCodemeta);
 
     document.querySelector('#inputForm')
         .addEventListener('change', generateCodemeta);
 
-    initAuthors()
+    initPersons('author', 'Author');
+    initPersons('contributor', 'Contributor');
 }
