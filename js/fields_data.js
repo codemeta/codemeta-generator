768caabb0768cf79e975914a792e0f1eb98de078
Kumar Shivendu <shivendu@iitbhilai.ac.in>
2023-04-10 20:02:33 +0530

    Add support for multiple license selection (#15)

--- js/fields_data.js
+++ js/fields_data.js
@@ -34,16 +34,45 @@ function initSpdx() {
     xhr.send();
 }
 
-function validateLicense() {
+function insertLicenseElement(licenseId) {
+    let selectedLicenses = document.getElementById("selected-licenses");
+    let newLicense = document.createElement("div");
+    newLicense.className = "selected-license";
+    newLicense.innerHTML = `
+        <span class="license-id">${licenseId}</span>
+        <button type="button" class="remove-license" onclick="removeLicense(this)">Remove</button>
+        `;
+
+    selectedLicenses.appendChild(newLicense);
+    return newLicense;
+}
+
+function validateLicense(e) {
+    // continue only if Enter/Tab key is pressed
+    if (e.keyCode && e.keyCode !== 13 && e.keyCode !== 9) {
+        return;
+    }
+    // Note: For some reason e.keyCode is undefined when Enter/Tab key is pressed.
+    // Maybe it's because of the datalist. But the above condition should
+    // work in either case.
+
     var licenseField = document.getElementById('license');
     var license = licenseField.value;
     if (SPDX_LICENSE_IDS !== null && SPDX_LICENSE_IDS.indexOf(license) == -1) {
         licenseField.setCustomValidity('Unknown license id');
     }
     else {
-        licenseField.setCustomValidity('');
-    }
+        insertLicenseElement(license);
 
+        licenseField.value = "";
+        licenseField.setCustomValidity('');
+        generateCodemeta();
+    }
+}
+
+function removeLicense(btn) {
+    btn.parentElement.remove();
+    generateCodemeta();
 }
 
 function initFieldsData() {
