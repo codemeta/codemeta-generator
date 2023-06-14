codemeta/codemeta-generator

commit 768caabb0768cf79e975914a792e0f1eb98de078

Add support for multiple license selection (#15)

cypress/integration/basics.js
@@ -34,20 +34,48 @@ describe('JSON Generation', function() {
         });
     });
 
-    it('works just from all main fields', function() {
+    it('works just from all main fields when using only one license', function() {
         cy.get('#name').type('My Test Software');
         cy.get('#description').type('This is a\ngreat piece of software');
         cy.get('#dateCreated').type('2019-10-02');
         cy.get('#datePublished').type('2020-01-01');
         cy.get('#license').type('AGPL-3.0');
+        cy.get("#license").type('{enter}');
         cy.get('#generateCodemeta').click();
 
+        cy.get("#license").should('have.value', '');
         cy.get('#errorMessage').should('have.text', '');
         cy.get('#codemetaText').then((elem) => JSON.parse(elem.text()))
             .should('deep.equal', {
                 "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
                 "@type": "SoftwareSourceCode",
                 "license": "https://spdx.org/licenses/AGPL-3.0",
+                "dateCreated": "2019-10-02",
+                "datePublished": "2020-01-01",
+                "name": "My Test Software",
+                "description": "This is a\ngreat piece of software",
+        });
+    });
+
+    it('works just from all main fields when using multiple licenses', function() {
+        cy.get('#name').type('My Test Software');
+        cy.get('#description').type('This is a\ngreat piece of software');
+        cy.get('#dateCreated').type('2019-10-02');
+        cy.get('#datePublished').type('2020-01-01');
+        cy.get('#license').type('AGPL-3.0');
+        cy.get("#license").type('{enter}');
+        cy.get('#license').type('MIT');
+        cy.get("#license").type('{enter}');
+
+        cy.get('#generateCodemeta').click();
+
+        cy.get("#license").should('have.value', '');
+        cy.get('#errorMessage').should('have.text', '');
+        cy.get('#codemetaText').then((elem) => JSON.parse(elem.text()))
+            .should('deep.equal', {
+                "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+                "@type": "SoftwareSourceCode",
+                "license": ["https://spdx.org/licenses/AGPL-3.0", "https://spdx.org/licenses/MIT"],
                 "dateCreated": "2019-10-02",
                 "datePublished": "2020-01-01",
                 "name": "My Test Software",
@@ -70,7 +98,7 @@ describe('JSON Import', function() {
         cy.get('#name').should('have.value', 'My Test Software');
     });
 
-    it('works just from all main fields', function() {
+    it('works just from all main fields when using license as string', function() {
         cy.get('#codemetaText').then((elem) =>
             elem.text(JSON.stringify({
                 "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
@@ -88,7 +116,33 @@ describe('JSON Import', function() {
         cy.get('#description').should('have.value', 'This is a\ngreat piece of software');
         cy.get('#dateCreated').should('have.value', '2019-10-02');
         cy.get('#datePublished').should('have.value', '2020-01-01');
-        cy.get('#license').should('have.value', 'AGPL-3.0');
+        cy.get('#license').should('have.value', '');
+        cy.get("#selected-licenses").children().should('have.length', 1);
+        cy.get("#selected-licenses").children().first().children().first().should('have.text', 'AGPL-3.0');
+    });
+
+    it('works just from all main fields when using license as array', function() {
+        cy.get('#codemetaText').then((elem) =>
+            elem.text(JSON.stringify({
+                "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+                "@type": "SoftwareSourceCode",
+                "license": ["https://spdx.org/licenses/AGPL-3.0", "https://spdx.org/licenses/MIT"],
+                "dateCreated": "2019-10-02",
+                "datePublished": "2020-01-01",
+                "name": "My Test Software",
+                "description": "This is a\ngreat piece of software",
+            }))
+        );
+        cy.get('#importCodemeta').click();
+
+        cy.get('#name').should('have.value', 'My Test Software');
+        cy.get('#description').should('have.value', 'This is a\ngreat piece of software');
+        cy.get('#dateCreated').should('have.value', '2019-10-02');
+        cy.get('#datePublished').should('have.value', '2020-01-01');
+        cy.get('#license').should('have.value', '');
+        cy.get("#selected-licenses").children().should('have.length', 2);
+        cy.get("#selected-licenses").children().eq(0).children().first().should('have.text', 'AGPL-3.0');
+        cy.get("#selected-licenses").children().eq(1).children().first().should('have.text', 'MIT');
     });
 
     it('errors on invalid type', function() {

js/dynamic_form.js
@@ -23,7 +23,7 @@ function createPersonFieldset(personPrefix, legend) {
     fieldset.classList.add("person");
     fieldset.classList.add("leafFieldset");
     fieldset.id = personPrefix;
-    
+
     fieldset.innerHTML = `
         <legend>${legend}</legend>
         <div class="moveButtons">
@@ -123,7 +123,7 @@ function removePerson(prefix) {
 
     document.querySelector(`#${prefix}_${personId}`).remove();
 
-    setNbPersons(prefix, personId-1);
+    setNbPersons(prefix, personId - 1);
 }
 
 // Initialize a group of persons (authors, contributors) on page load.
@@ -149,6 +149,8 @@ function removePersons(prefix) {
 function resetForm() {
     removePersons('author');
     removePersons('contributor');
+    // Reset the list of selected licenses
+    document.getElementById("selected-licenses").innerHTML = '';
 
     // Reset the form after deleting elements, so nbPersons doesn't get
     // reset before it's read.
@@ -161,7 +163,7 @@ function fieldToLower(event) {
 
 function initCallbacks() {
     document.querySelector('#license')
-        .addEventListener('change', validateLicense);
+        .addEventListener('keyup', validateLicense);
 
     document.querySelector('#generateCodemeta')
         .addEventListener('click', generateCodemeta);

index.html
@@ -71,7 +71,7 @@ See top-level LICENSE file for more information
             </p>
 
             <p>
-                <label for="license">License</label>
+                <label for="license">License(s)</label>
                 <input list="licenses" name="license" id="license"
                     aria-describedby="licenses_descr"> <!-- TODO: insert placeholder -->
 
@@ -81,6 +81,10 @@ See top-level LICENSE file for more information
 
                 <br />
                 <span class="field-description" id="licenses_descr">from <a href="https://spdx.org/license-list">SPDX licence list</a></span>
+
+                <div id="selected-licenses">
+                    <!-- This div is to be filled as the user selects licenses -->
+                </div>
             </p>
         </fieldset>
 

js/codemeta_generation.js
@@ -26,6 +26,11 @@ function setIfDefined(query, value) {
     }
 }
 
+function getLicenses() {
+    let selectedLicenses = Array.from(document.getElementById("selected-licenses").children);
+    return selectedLicenses.map(licenseDiv => SPDX_PREFIX + licenseDiv.children[0].innerText);
+}
+
 // Names of codemeta properties with a matching HTML field name
 const directCodemetaFields = [
     'codeRepository',
@@ -120,9 +125,9 @@ function generateCodemeta() {
             "@type": "SoftwareSourceCode",
         };
 
-        var license = getIfSet('#license')
-        if (license !== undefined) {
-            doc["license"] = SPDX_PREFIX + getIfSet('#license');
+        let licenses = getLicenses();
+        if (licenses.length > 0) {
+            doc["license"] = (licenses.length === 1) ? licenses[0] : licenses;
         }
 
         // Generate most fields
@@ -210,9 +215,16 @@ function importCodemeta() {
     var doc = parseAndValidateCodemeta(false);
     resetForm();
 
-    if (doc['license'] !== undefined && doc['license'].indexOf(SPDX_PREFIX) == 0) {
-        var license = doc['license'].substring(SPDX_PREFIX.length);
-        document.querySelector('#license').value = license;
+    if (doc['license'] !== undefined) {
+        if (typeof doc['license'] === 'string') {
+            doc['license'] = [doc['license']];
+        }
+
+        doc['license'].forEach(l => {
+            if (l.indexOf(SPDX_PREFIX) !== 0) { return; }
+            let licenseId = l.substring(SPDX_PREFIX.length);
+            insertLicenseElement(licenseId);
+        });
     }
 
     directCodemetaFields.forEach(function (item, index) {

js/fields_data.js
@@ -34,16 +34,45 @@ function initSpdx() {
     xhr.send();
 }
 
-function validateLicense() {
+function insertLicenseElement(licenseId) {
+    let selectedLicenses = document.getElementById("selected-licenses");
+    let newLicense = document.createElement("div");
+    newLicense.className = "selected-license";
+    newLicense.innerHTML = `
+        <span class="license-id">${licenseId}</span>
+        <button type="button" class="remove-license" onclick="removeLicense(this)">Remove</button>
+        `;
+
+    selectedLicenses.appendChild(newLicense);
+    return newLicense;
+}
+
+function validateLicense(e) {
+    // continue only if Enter/Tab key is pressed
+    if (e.keyCode && e.keyCode !== 13 && e.keyCode !== 9) {
+        return;
+    }
+    // Note: For some reason e.keyCode is undefined when Enter/Tab key is pressed.
+    // Maybe it's because of the datalist. But the above condition should
+    // work in either case.
+
     var licenseField = document.getElementById('license');
     var license = licenseField.value;
     if (SPDX_LICENSE_IDS !== null && SPDX_LICENSE_IDS.indexOf(license) == -1) {
         licenseField.setCustomValidity('Unknown license id');
     }
     else {
-        licenseField.setCustomValidity('');
-    }
+        insertLicenseElement(license);
 
+        licenseField.value = "";
+        licenseField.setCustomValidity('');
+        generateCodemeta();
+    }
+}
+
+function removeLicense(btn) {
+    btn.parentElement.remove();
+    generateCodemeta();
 }
 
 function initFieldsData() {

main.css
@@ -76,3 +76,7 @@ input[type=URL] {
 input:invalid {
     color: red;
 }
+
+.selected-license {
+    margin: 2px;
+}

commit 6402deecf6b6bd08ac0c173e024cb873fef70018

Fix breakage of keyboard-less license selection
Broken by 768caabb0768cf79e975914a792e0f1eb98de078.

cypress/integration/basics.js
@@ -82,6 +82,29 @@ describe('JSON Generation', function() {
                 "description": "This is a\ngreat piece of software",
         });
     });
+
+    it('works when choosing licenses without the keyboard', function() {
+        cy.get('#name').type('My Test Software');
+        cy.get('#description').type('This is a\ngreat piece of software');
+        cy.get('#dateCreated').type('2019-10-02');
+        cy.get('#datePublished').type('2020-01-01');
+        cy.get('#license').type('AGPL-3.0');
+        // no cy.get("#license").type('{enter}'); here
+        cy.get('#generateCodemeta').click();
+
+        cy.get("#license").should('have.value', '');
+        cy.get('#errorMessage').should('have.text', '');
+        cy.get('#codemetaText').then((elem) => JSON.parse(elem.text()))
+            .should('deep.equal', {
+                "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+                "@type": "SoftwareSourceCode",
+                "license": "https://spdx.org/licenses/AGPL-3.0",
+                "dateCreated": "2019-10-02",
+                "datePublished": "2020-01-01",
+                "name": "My Test Software",
+                "description": "This is a\ngreat piece of software",
+        });
+    });
 });
 
 describe('JSON Import', function() {

js/dynamic_form.js
@@ -163,7 +163,7 @@ function fieldToLower(event) {
 
 function initCallbacks() {
     document.querySelector('#license')
-        .addEventListener('keyup', validateLicense);
+        .addEventListener('change', validateLicense);
 
     document.querySelector('#generateCodemeta')
         .addEventListener('click', generateCodemeta);